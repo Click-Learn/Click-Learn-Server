fe160a8afccc85dba9b670c55cc8565ac9d45fc7
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-04-07 15:45:45 +0300

    ddownloaded mysql2

--- 2-utils/dal.ts
+++ 2-utils/dal.ts
@@ -2,20 +2,20 @@ import * as dotenv from 'dotenv';
 
 dotenv.config({ path: ".env" });
 
-// import mysql, { RowDataPacket } from "mysql2/promise";
+import mysql, { RowDataPacket } from "mysql2/promise";
 
-// const pool = mysql.createPool({
-//     host:  process.env.DB_HOST,
-//     user: process.env.DB_USERNAME,
-//     port: +process.env.DB_PORT,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PASSWORD
-//     // waitForConnections: true,
-//     // connectionLimit: 10,
-//     // queueLimit: 0
-//   });
+const pool = mysql.createPool({
+    host:  process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    port: +process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
+    // waitForConnections: true,
+    // connectionLimit: 10,
+    // queueLimit: 0
+  });
 
-//   export function execute<T>(query: string, params?: any[]){
-//     return pool.execute<T & RowDataPacket[]>(query, params)
-//   }
+  export function execute<T>(query: string, params?: any[]){
+    return pool.execute<T & RowDataPacket[]>(query, params)
+  }
 
