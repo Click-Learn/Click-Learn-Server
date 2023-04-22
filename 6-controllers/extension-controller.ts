f9d1c8558bff87a1125e8a978a767697a31c34d5
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-04-22 09:58:15 +0300

    removed test function

--- 6-controllers/extension-controller.ts
+++ 6-controllers/extension-controller.ts
@@ -9,40 +9,15 @@ export const extenstionRouter = express.Router();
 
 extenstionRouter.post('/translateTheWord', async (req, res, next) => {
   try {
-    // const token = req.headers.authorization;
-    // const user: UserModel = jwt_decode(token);הןה
-
     const { word } = req.body;
     const hebrewWord = await translateText(word);
 
-    // const result = await Register(user.email);
-    // console.log(`Registration result: ${JSON.stringify(result)}`);
     res.json(hebrewWord).status(200);
   } catch (e) {
     console.log(`Error: ${e.message}`);
     res.status(500).send(`Failed to tranlsate the word: ${e.message}`);
   }
 });
-  
-// extenstionRouter.post('/saveWordromExtenstion', async (req, res, next) => {
-//   try {
-
-//     const { hebrewWord, englishWord, email } = req.body;
-
-//      const userId = await getUserIdByEmail(email);
-//     if(userId) {
-//       // saveWordstoUser(userId,hebrewWord, englishWord )
-//     } else {
-//       // register a new user then do it again
-//     }
-//     // const result = await Register(user.email);
-//     // console.log(`Registration result: ${JSON.stringify(result)}`);
-//     res.json(hebrewWord).status(200);
-//   } catch (e) {
-//     console.log(`Error: ${e.message}`);
-//     res.status(500).send(`Failed to save the word: ${e.message}`);
-//   }
-// });
   
 extenstionRouter.post('/saveWordromExtenstion', async (req, res, next) => {
   console.log("test");
